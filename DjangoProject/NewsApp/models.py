ca3f7f9588a0cb8424eb829f3a62d73d1ee9f24c
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-06-01 11:07:41 +0000

    bd edited

--- DjangoProject/NewsApp/models.py
+++ DjangoProject/NewsApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class News(models.Model):
+    author = models.CharField(default= 'Name', max_length=30)
+    title = models.CharField(default= "Title", max_length=30)
+    description = models.TextField()
+
+
+class Sports(models.Model):
+    author = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+
+
+
+
+
+
+
+
+
+
