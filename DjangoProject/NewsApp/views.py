c05dd4bc6f81a0f8d537151f0f038d7f08554c02
Franklin Adjei <57026567+Hotlynn2@users.noreply.github.com>
2021-06-27 19:57:04 +0000

    retrieving data from db

--- DjangoProject/NewsApp/views.py
+++ DjangoProject/NewsApp/views.py
@@ -1,5 +1,6 @@
+# from DjangoProject import NewsApp
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from .models import News
 
 # Create your views here.
 
@@ -7,20 +8,34 @@ def Home(request):
 
     context = {
     
-        'name' : 'Franklin'
+        'name' : 'Franklin',
+        'height' : 171.5
     }
 
     return render(request, 'home.html', context)
 
 
-def News(request):
+def NewsP(request):
+
+    obj = News.objects.get(id= 2)
 
     context = {
-        'programming_list' : ['Python', 'PhP', 'Swift', 'JavaScript']
+        'programming_list' : ['Python', 'PhP', 'Swift', 'JavaScript'],
+        'data' : obj
     }
 
     return render(request, 'news.html', context)
 
 
-def Contact(request):
+def Contact(request):  
+
+    obj = News.objects.get(id= 1)
+
+    context = {
+        
+        'new' : obj
+    }
+
+
+
     return render(request, 'contact.html')
